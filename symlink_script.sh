6964bfb60af6597f2476b8474564a806c31db4e6
Mo Zhu <mozhu888@gmail.com>
2014-12-30 22:06:20 -0500

    symlink script

---
+++ symlink_script.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+ln -svfi ~/Dropbox/dotfiles/.bashrc ~/
+ln -svfi ~/Dropbox/dotfiles/.vimrc ~/
+ln -svfi ~/Dropbox/dotfiles/.bash_profile ~/
+ln -svfi ~/Dropbox/dotfiles/.gemrc ~/
+ln -svfi ~/Dropbox/dotfiles/.git ~/
+ln -svfi ~/Dropbox/dotfiles/.gitconfig ~/
+ln -svfi ~/Dropbox/dotfiles/.irbrc ~/
+ln -svfi ~/Dropbox/dotfiles/.profile ~/
+ln -svfi ~/Dropbox/dotfiles/.pryrc ~/
+ln -svfi ~/Dropbox/dotfiles/.rspec ~/
