5763f6ef94ef341d3d4b1deaee1ce2f05f763177
Mo Zhu <mozhu888@gmail.com>
2025-12-29 05:32:13 -0800

    switch from dropbox to icloud

--- symlink_script.sh
+++ symlink_script.sh
@@ -17,6 +17,12 @@ echo "Cloning vundle into ~/.vim"
 echo "If vundle already exists in ~/.vim, then this will give a failure message."
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/Vundle.vim
 
+echo "Shortcut to ~/iCloud"
+ln -s "/Users/mozhu/Library/Mobile Documents/com~apple~CloudDocs/" ~/iCloud
+
+echo "Shortcut to ~/programming"
+ln -s "/Users/mozhu/Library/Mobile Documents/com~apple~CloudDocs/programming/" ~/programming
+
 echo "Script done."
 echo "Next when you open vim, run the command :BundleInstall to install the plugins."
 echo "You can also add your own plugins to the list."
